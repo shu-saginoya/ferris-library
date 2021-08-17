3e7ea764d6f03eada5c1f44904141e523d3f7ded
shu-saginoya <saginoya@create-nippo.co.jp>
2021-08-17 15:43:01 +0900

    画像などの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/css/variables.scss'],
     theme: {
       dark: false,
       themes: {
