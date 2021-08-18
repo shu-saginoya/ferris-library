920098db9230b03e69320837e276dffe0e1d7ba8
shu-saginoya <saginoya@create-nippo.co.jp>
2021-08-18 13:20:29 +0900

    トップページにカルーセルを追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,37 @@
 <template>
-  <v-main> あいう </v-main>
+  <v-main>
+    <v-carousel
+      cycle
+      height="440px"
+      hide-delimiter-background
+    >
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+  </v-main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: require('@/assets/image/img_02.png'),
+        },
+        {
+          src: require('@/assets/image/img_07.png'),
+        },
+        {
+          src: require('@/assets/image/img_09.png'),
+        },
+      ],
+    }
+  },
+}
+</script>
