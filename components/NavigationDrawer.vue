344e08404ccbb03d05f74fabaf9a7aa89c1ffe21
shu-saginoya <saginoya@create-nippo.co.jp>
2021-08-20 15:30:29 +0900

    ドロワーメニューについて別タブで開くリンクにも対応。また別タブ及びPDFファイルのアイコン表示

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -1,25 +1,40 @@
 <template>
   <v-list>
     <template v-for="menu in menus">
-      <v-list-item v-if="menu.link" :key="menu.category - eng" :to="menu.link">
+      <v-list-item v-if="menu.push" :key="menu.category-eng" :to="menu.push">
         <v-list-item-icon>
           <v-icon>{{ menu.icon }}</v-icon>
         </v-list-item-icon>
         <v-list-item-title>{{ menu.category }}</v-list-item-title>
       </v-list-item>
-      <v-list-group v-else :key="menu.category - eng" :prepend-icon="menu.icon">
+      <v-list-group v-else :key="menu.category-eng" :prepend-icon="menu.icon">
         <template #activator>
           <v-list-item-title>{{ menu.category }}</v-list-item-title>
         </template>
 
+        <template v-for="content in menu.contents">
         <v-list-item
-          v-for="content in menu.contents"
+          v-if="content.push"
           :key="content.eng"
-          :to="content.link"
+          :to="content.push"
           link
         >
           <v-list-item-title v-text="content.title"></v-list-item-title>
         </v-list-item>
+
+        <v-list-item
+          v-if="content.href"
+          :key="content.eng"
+          :href="content.href"
+          target="_blank"
+          link
+        >
+          <v-list-item-title v-text="content.title"></v-list-item-title>
+          <v-list-item-icon>
+            <v-icon v-text="content.icon"></v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+        </template>
       </v-list-group>
     </template>
   </v-list>
