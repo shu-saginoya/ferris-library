848a03be5975f9558324231fdd0383512ec4b17f
shu-saginoya <saginoya@create-nippo.co.jp>
2021-08-24 15:08:07 +0900

    メインビジュアルの修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <template>
   <v-main>
-    <v-carousel
-      cycle
-      height="440px"
-      hide-delimiter-background
-    >
+    <v-carousel cycle height="440px" hide-delimiter-background>
       <v-carousel-item
         v-for="(item, i) in items"
         :key="i"
         :src="item.src"
         reverse-transition="fade-transition"
         transition="fade-transition"
-      ></v-carousel-item>
+        class="mein-visual"
+      >
+        <v-row class="fill-height" align="center" justify="center">
+          <img src="@/assets/image/site-logo.png" width="403" height="130" />
+        </v-row>
+      </v-carousel-item>
     </v-carousel>
+    <div>
+      あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />あ<br />
+    </div>
   </v-main>
 </template>
 
@@ -22,16 +26,37 @@ export default {
     return {
       items: [
         {
-          src: require('@/assets/image/img_02.png'),
+          src: require('@/assets/image/main-visual/main-visual-01.jpg'),
         },
         {
-          src: require('@/assets/image/img_07.png'),
+          src: require('@/assets/image/main-visual/main-visual-02.jpg'),
         },
         {
-          src: require('@/assets/image/img_09.png'),
+          src: require('@/assets/image/main-visual/main-visual-03.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-04.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-05.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-06.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-07.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-08.jpg'),
         },
       ],
     }
   },
 }
 </script>
+
+<style scoped>
+.mein-visual {
+  filter: brightness(110%) grayscale(10%);
+}
+</style>
