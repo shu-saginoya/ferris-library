f6b7086e2d4829b307f9d6dbbff7da5bf0621c06
shu-saginoya <saginoya@create-nippo.co.jp>
2021-09-01 13:56:49 +0900

    ヘッダーやドロワー確認用バージョンの書き出し用

--- components/MainVisual.vue
+++ components/MainVisual.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-carousel
-    cycle
-    height="440px"
-    hide-delimiter-background
-  >
+  <v-carousel cycle height="440" hide-delimiter-background show-arrows-on-hover>
     <v-carousel-item
       v-for="(item, i) in items"
       :key="i"
@@ -19,33 +15,35 @@
 <script>
 export default {
   name: 'MainVisual',
-  data: () => ({
-    items: [
-      {
-        src: require('@/assets/image/main-visual/main-visual-01.jpg'),
-      },
-      {
-        src: require('@/assets/image/main-visual/main-visual-02.jpg'),
-      },
-      {
-        src: require('@/assets/image/main-visual/main-visual-03.jpg'),
-      },
-      {
-        src: require('@/assets/image/main-visual/main-visual-04.jpg'),
-      },
-      {
-        src: require('@/assets/image/main-visual/main-visual-06.jpg'),
-      },
-      {
-        src: require('@/assets/image/main-visual/main-visual-05.jpg'),
-      },
-      {
-        src: require('@/assets/image/main-visual/main-visual-07.jpg'),
-      },
-      {
-        src: require('@/assets/image/main-visual/main-visual-08.jpg'),
-      },
-    ],
-  }),
+  data() {
+    return {
+      items: [
+        {
+          src: require('@/assets/image/main-visual/main-visual-01.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-02.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-03.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-04.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-06.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-05.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-07.jpg'),
+        },
+        {
+          src: require('@/assets/image/main-visual/main-visual-08.jpg'),
+        },
+      ],
+    }
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <v-main>
-    <v-container>
-      <main-visual />
-      <section>
-        <h2 class="v-heading text-h4 pt-6 mb-2">Calender</h2><p>※調整中です</p>
-        <v-row justify="space-around">
-          <v-col cols="12" md="6" class="pa-6"><calender /></v-col>
-          <v-col cols="12" md="6" class="pa-6"><calender /></v-col>
-        </v-row>
-      </section>
-      <section>
-        <h2 class="v-heading text-h4 pt-6 mb-2">News</h2>
-      </section>
-    </v-container>
+    <main-visual />
+    <!--<v-container tag="section">
+      <h2 class="v-heading text-h4 pt-8 mb-2">Calender※調整中です</h2>
+      <v-row justify="space-around">
+        <v-col cols="12" md="6" class="pa-6"><calender /></v-col>
+        <v-col cols="12" md="6" class="pa-6"><calender /></v-col>
+      </v-row>
+    </v-container>-->
+    <!--<v-container tag="section">
+      <h2 class="v-heading text-h4 pt-6 mb-2">News</h2>
+    </v-container>-->
   </v-main>
 </template>
 
