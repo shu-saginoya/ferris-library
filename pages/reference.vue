61a7b126696dc0bacf044ab63ff178daf6936de9
shu-saginoya <saginoya@create-nippo.co.jp>
2021-10-06 16:06:57 +0900

    リファレンスページの制作

---
+++ pages/reference.vue
@@ -0,0 +1,257 @@
+<template>
+  <v-container>
+    <base-page-title>レファレンスサービス</base-page-title>
+
+    <base-sub-title>サービス一覧</base-sub-title>
+
+    <v-card>
+      <v-card-title> 所蔵調査 </v-card-title>
+      <v-card-text>
+        ご希望の資料が当図書館にない時、国内外の図書館や資料館の所蔵調査をいたします。学部生はMyLibraryからも調査の依頼ができます。
+      </v-card-text>
+      <v-card-actions>
+        <base-default-dialog>
+          <template #name>文献複写</template>
+          <template #contents>
+            <div class="text-h5 pt-12">文献複写</div>
+            <div class="text-h6 pt-6">サービス内容</div>
+            <ul>
+              <li>当館にない文献のコピーを取り寄せることができます。</li>
+              <li>
+                資料名、掲載巻号（雑誌）、ページなどの情報を申込用紙（MyLibrary
+                にログインのうえ入手できます）にご記入の上、レファレンスカウンターにお申し込みください。大学院生・教員はMyLibraryからも申し込みができます。
+              </li>
+            </ul>
+            <div class="text-h6 pt-6">所要期間</div>
+            <p>4日～</p>
+            <div class="text-h6 pt-6">費用</div>
+            <p>コピー代金・送料・手数料等</p>
+          </template>
+          <template #actions>
+            <base-btn-my-library></base-btn-my-library>
+          </template>
+        </base-default-dialog>
+
+        <base-default-dialog>
+          <template #name>現物貸借</template>
+          <template #contents>
+            <div class="text-h5 pt-12">現物貸借</div>
+            <div class="text-h6 pt-6">サービス内容</div>
+            <ul>
+              <li>
+                当館にない図書を学外の図書館から一定期間借りることができます。
+              </li>
+              <li>
+                申込用紙（MyLibrary
+                にログインのうえ入手できます）にご記入のうえ、レファレンスカウンターにお申し込みください。大学院生・教員はMyLibraryからも申し込みができます。
+              </li>
+              <li>
+                館内でのみ閲覧することができます。館外への持ち出しはできません。
+              </li>
+              <li>
+                その他の利用方法については、所蔵館の規則によって異なります。
+              </li>
+              <li>
+                所蔵館の規定や資料の劣化等により取り寄せができない場合もあります。
+              </li>
+            </ul>
+            <div class="text-h6 pt-6">所要期間</div>
+            <p>4日～</p>
+            <div class="text-h6 pt-6">費用</div>
+            <p>往復送料・手数料等</p>
+          </template>
+          <template #actions>
+            <base-btn-my-library></base-btn-my-library>
+          </template>
+        </base-default-dialog>
+
+        <base-btn-my-library></base-btn-my-library>
+      </v-card-actions>
+    </v-card>
+
+    <v-card class="mt-4">
+      <v-card-title> 紹介状 </v-card-title>
+      <v-card-text>
+        他大学の図書館を利用するために必要な紹介状を発行します。
+      </v-card-text>
+      <v-card-actions>
+        <base-default-dialog>
+          <template #name>大学図書館</template>
+          <template #contents>
+            <div class="text-h5 pt-12">大学図書館</div>
+            <div class="text-h6 pt-6">注意事項</div>
+            <p>
+              当館からの問い合わせと紹介状が必要です。カウンターに希望の利用日と資料名をお知らせください。資料を借りることはできませんが、館内で閲覧したりコピーしたりすることができます。
+            </p>
+            <p>
+              問い合わせに時間がかかりますので、お早めにお申し込みください。また、先方に直接問い合わせないようお願いいたします。
+            </p>
+            <p>蔵書検索は「CiNii Books」から行ってください。</p>
+          </template>
+          <template #actions>
+            <base-btn-open-in-new
+              link="CiNii Books"
+              url="http://ci.nii.ac.jp/books/"
+            ></base-btn-open-in-new>
+          </template>
+        </base-default-dialog>
+
+        <base-default-dialog>
+          <template #name>横浜市立図書館</template>
+          <template #contents>
+            <div class="text-h5 pt-12">横浜市立図書館</div>
+            <div class="text-h6 pt-6">注意事項</div>
+            <p>
+              自由に利用することができます。図書館カードを作れば、資料を借りることもできますのでお勧めします。
+            </p>
+            <p>ホームページで開館日などを確認できます。</p>
+            <p>
+              蔵書検索は「横浜市立図書館蔵書検索ページ」から行ってください。
+            </p>
+          </template>
+          <template #actions>
+            <base-btn-open-in-new
+              link="横浜市立図書館蔵書検索ページ"
+              url="https://opac.lib.city.yokohama.lg.jp/opac/"
+            ></base-btn-open-in-new>
+          </template>
+        </base-default-dialog>
+
+        <base-default-dialog>
+          <template #name>神奈川県立図書館</template>
+          <template #contents>
+            <div class="text-h5 pt-12">神奈川県立図書館</div>
+            <div class="text-h6 pt-6">注意事項</div>
+            <p>
+              自由に利用することができます。図書館カードを作れば、資料を借りることもできますのでお勧めします。
+            </p>
+            <p>ホームページで開館日などを確認できます。</p>
+          </template>
+          <template #actions>
+            <base-btn-open-in-new
+              link="神奈川県立の図書館"
+              url="https://www.klnet.pref.kanagawa.jp/"
+            ></base-btn-open-in-new>
+          </template>
+        </base-default-dialog>
+
+        <base-default-dialog>
+          <template #name>国立国会図書館</template>
+          <template #contents>
+            <div class="text-h5 pt-12">国立国会図書館</div>
+            <div class="text-h6 pt-6">注意事項</div>
+            <p>
+              自由に利用できますが、閉架式なので直接書架を見ることはできません。利用したい資料を申し出て出納してもらうので、事前に希望の資料を調べてから訪問してください。また、個人への貸出は行っていません。
+            </p>
+            <p>蔵書検索は「国立国会図書館サーチ」から行ってください。</p>
+          </template>
+          <template #actions>
+            <base-btn-open-in-new
+              link="国立国会図書館サーチ"
+              url="http://iss.ndl.go.jp/"
+            ></base-btn-open-in-new>
+          </template>
+        </base-default-dialog>
+      </v-card-actions>
+    </v-card>
+
+    <v-card class="mt-4">
+      <v-card-title> 共通閲覧証 </v-card-title>
+      <v-card-text>
+        紹介状なしで他大学の図書館を利用できる共通閲覧証を発行します。
+      </v-card-text>
+      <v-card-actions @click="show = !show">
+        <v-btn color="primary" text> 詳細をみる </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+          <v-card-text>
+            下記の共通閲覧証があれば、紹介状なしで加盟大学図書館・機関が利用できます。カウンターで毎年度発行手続きをしてください。
+          </v-card-text>
+          <v-card-actions>
+            <base-default-dialog>
+              <template #name>神奈川県内大学図書館共通閲覧証</template>
+              <template #contents>
+                <div class="text-h5 pt-12">神奈川県内大学図書館共通閲覧証</div>
+                <div class="text-h6 pt-6">対象館</div>
+                <p>神奈川県図書館協会加盟の大学図書館</p>
+                <div class="text-h6 pt-6">対象者</div>
+                <p>教員・院生・学部生</p>
+              </template>
+              <template #actions>
+                <base-btn-open-in-new
+                  link="神奈川県図書館協会"
+                  url="http://www.kanagawa-la.jp/link/"
+                ></base-btn-open-in-new>
+              </template>
+            </base-default-dialog>
+
+            <base-default-dialog>
+              <template #name>音楽図書館協議会共通閲覧証</template>
+              <template #contents>
+                <div class="text-h5 pt-12">音楽図書館協議会共通閲覧証</div>
+                <div class="text-h6 pt-6">対象館</div>
+                <p>音楽図書館協議会加盟館</p>
+                <div class="text-h6 pt-6">対象者</div>
+                <p>音楽学部所属の教員・院生・学部生</p>
+              </template>
+              <template #actions>
+                <base-btn-open-in-new
+                  link="音楽図書館協議会"
+                  url="https://mlaj.org/"
+                ></base-btn-open-in-new>
+              </template>
+            </base-default-dialog>
+          </v-card-actions>
+        </div>
+      </v-expand-transition>
+    </v-card>
+
+    <v-card class="mt-4">
+      <v-card-title> 事項調査 </v-card-title>
+      <v-card-text>
+        調べものをしていてどの資料を見れば良いのか分からないとき、適切な資料をご紹介します。学部生はMyLibraryからも調査の依頼ができます。
+      </v-card-text>
+    </v-card>
+
+    <v-card class="mt-4">
+      <v-card-title> ツアー・ガイダンス </v-card-title>
+      <v-card-text>
+        先生方や学生の皆さんのご要望に応じて、図書館の効率的な使い方をご説明します。
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" text to="tour">
+          ツアー・ガイダンスページへ
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card class="mt-4">
+      <v-card-title>
+        横浜市内コンソーシアム加盟大学の図書館を利用する
+      </v-card-title>
+      <v-card-text>
+        横浜市内大学図書館コンソーシアムの加盟館は、原則として学生証又は大学発行の身分証明書を提示して手続きすることで、紹介状なしで館内利用ができます。<br />
+        手続き方法、利用できるサービスについては大学によって異なりますので、必ず各館のホームページで条件を確認してください。
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" text to="consortium">
+          詳細ページへ
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+}
+</script>
