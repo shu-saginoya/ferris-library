5caf52741bdbe4cf4e55983ac5fdd87d8b822092
shu-saginoya <saginoya@create-nippo.co.jp>
2021-10-06 16:06:38 +0900

    コンポーネントBaseBtnOpenInNewを制作。外部リンクを一括管理できるようにしたかったが断念。

---
+++ components/BaseBtnOpenInNew.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn :href="url" target="_blank" class="btn-open-in-new">
+    {{ link }}
+    <v-icon right>mdi-open-in-new</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'BaseBtnOpenInNew',
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.btn-open-in-new {
+  text-transform: none;
+}
+</style>
