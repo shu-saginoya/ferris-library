72dbd313ad868f4614ffcc49825eab8d8668cb87
shu-saginoya <saginoya@create-nippo.co.jp>
2021-10-15 15:52:27 +0900

    vue-pdfプラグインを導入。問題は多いがとりあえずPDFファイルが表示できるようになった

---
+++ components/DisplayPdf.vue
@@ -0,0 +1,11 @@
+<template>
+    <client-only>
+      <vue-pdf src="/pdf/newspaper.pdf" />
+    </client-only>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ pages/pdf.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    <h1 class="title">sample-vue-pdf</h1>
+    <display-pdf></display-pdf>
+  </v-container>
+</template>
+
+<script>
+</script>
