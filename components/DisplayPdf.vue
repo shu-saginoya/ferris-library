cb6ea2512af28f9c7cd94df5a232b13a13ea30f6
shu-saginoya <saginoya@create-nippo.co.jp>
2021-10-18 15:41:04 +0900

    コンポーネントDisplayPdfの修正

--- components/DisplayPdf.vue
+++ components/DisplayPdf.vue
@@ -1,11 +1,25 @@
 <template>
-    <client-only>
-      <vue-pdf src="/pdf/newspaper.pdf" />
-    </client-only>
+  <v-sheet color="grey lighten-3" class="pa-6">
+    <v-pagination
+      v-model="currentPageNum"
+      :length="totalPageNum"
+      total-visible="6"
+      class="mb-2"
+    ></v-pagination>
+    <vue-pdf
+      src="/pdf/newspaper.pdf"
+      :page="currentPageNum"
+      @num-pages="totalPageNum = $event"
+    ></vue-pdf>
+  </v-sheet>
 </template>
 
 <script>
-import Vue from 'vue'
-
-export default Vue.extend({})
+export default {
+  name: 'DisplayPdf',
+  data: () => ({
+    currentPageNum: 1,
+    totalPageNum: 0,
+  }),
+}
 </script>
