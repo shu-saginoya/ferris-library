038eafe315d46490ab787fd2389f5909593da42b
shu-saginoya <saginoya@create-nippo.co.jp>
2021-10-20 15:42:51 +0900

    年間予定ページの制作

---
+++ pages/schedule.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>
+        <base-page-title>年間予定</base-page-title>
+        <v-card>
+            <v-card-text>
+                2021年度のスケジュールは開館カレンダーをご確認ください
+            </v-card-text>
+            <v-card-actions>
+                <base-btn-inside link="開館カレンダーへ" to="calender"></base-btn-inside>
+            </v-card-actions>
+        </v-card>
+    </v-container>
+</template>
