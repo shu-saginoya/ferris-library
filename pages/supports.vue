e0ce06b6835bfbd58d0e5bfbbc46ea7c68ef3db2
shu-saginoya <saginoya@create-nippo.co.jp>
2021-10-21 14:26:42 +0900

    障がいのある方への支援ページを制作

---
+++ pages/supports.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-container>
+    <base-page-title>障がいのある方への支援</base-page-title>
+    <p>図書館では、障がいのある方に以下の支援を行っています。</p>
+
+    <v-card>
+      <v-card-title @click="show = !show">
+        図書館職員による支援
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn></v-card-title
+      >
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+          <v-card-text>
+            <div class="text-h6 pt-3">代行検索</div>
+            <p>
+              OPACやデータベースの利用が困難な場合、図書館員が代わりに検索します。
+            </p>
+            <div class="text-h6 pt-3">書架案内</div>
+            <p>
+              高い書棚や、書棚と書棚の間の通路が狭いなど、本の取り出しが困難な場合、図書館員が代行します。
+            </p>
+            <div class="text-h6 pt-3">館内移動援助</div>
+            <p>
+              館内はバリアフリー対応ですが、状況に応じ図書館員が介助します。
+            </p>
+            <div class="text-h6 pt-3">入退館援助</div>
+            <p>出入口の通行が困難な場合は、図書館員が介助します。</p>
+            <div class="text-h6 pt-3">機器利用援助</div>
+            <p>
+              コピー機、プリンター、PC、AV機器など、館内機器の利用が困難な場合は、図書館員が介助します。
+            </p>
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+
+    <v-card class="mt-3">
+      <v-card-title @click="show2 = !show2">
+        施設設備
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>{{ show2 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn></v-card-title
+      >
+      <v-expand-transition>
+        <div v-show="show2">
+          <v-divider></v-divider>
+          <v-card-text>
+            <div class="text-h6 pt-3">拡大書架ガイド</div>
+            <p>全ての書棚に拡大文字の書架ガイドを貼付しています。</p>
+            <div class="text-h6 pt-3">拡大読書器</div>
+            <p>3階の院生・教員閲覧室に拡大読書器を設置しています。</p>
+            <div class="text-h6 pt-3">音声読書器</div>
+            <p>
+              3階の検索コーナーに音声読書器（『よめーる』）を設置しています。
+            </p>
+            <div class="text-h6 pt-3">多目的トイレ</div>
+            <p>2階、3階、4階に多目的トイレがあります。</p>
+            <div class="text-h6 pt-3">プリンター</div>
+            <p>
+              1階のオンデマンドプリンターには、タッチパネルを搭載、低位置に設置しています。
+            </p>
+            <div class="text-h6 pt-3">点字</div>
+            <p>
+              エレベーター内のボタンに、点字を貼付しています。階段の手すりに、フロアを示す点字を貼付しています。
+            </p>
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+
+    <v-alert dense type="info" class="mt-6">
+      上記以外にもサポートが必要でしたら、図書館スタッフまでお申し出ください。できる限りご要望にお応えします。
+    </v-alert>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: true,
+    show2: true,
+  }),
+}
+</script>
