a2611c3f3b392736eaf7a11db6db6c035642902d
shu-saginoya <saginoya@create-nippo.co.jp>
2021-11-02 13:04:21 +0900

    コンポーネントNewsListの修正

--- components/NewsList.vue
+++ components/NewsList.vue
@@ -1,76 +1,74 @@
 <template>
   <v-card>
-    <v-skeleton-loader type="list-item" :loading="loading" max-width="200">
+    <v-skeleton-loader
+      type="list-item, divider, list-item-two-line, divider, list-item-two-line, divider, list-item-two-line, divider, list-item-two-line, divider, list-item-two-line"
+      :loading="loading"
+    >
       <v-subheader><slot name="name">News</slot></v-subheader>
     </v-skeleton-loader>
 
-    <v-list two-line>
+    <v-list>
       <template v-for="info in displayLists">
         <v-divider :key="info.id"></v-divider>
-        <v-skeleton-loader
-          :key="info.id"
-          type="list-item-two-line"
-          :loading="loading"
-        >
-          <v-list-item :key="info.id">
-            <v-list-item-content>
-              <v-list-item-title>{{ info.title }}</v-list-item-title>
-              <v-list-item-subtitle>
-                {{ info.date }}
-              </v-list-item-subtitle>
-            </v-list-item-content>
-
-            <v-list-item-action>
-              <v-dialog :key="info.id" v-model="dialog" max-width="600">
-                <template #activator="{ on, attrs }">
-                  <v-btn
-                    class="dialog-btn ma-2"
-                    color="primary lighten-1"
-                    v-bind="attrs"
-                    icon
-                    v-on="on"
+        <v-list-item :key="info.id" two-line>
+          <v-list-item-content>
+            <v-list-item-title class="wrap-text">{{
+              info.title
+            }}</v-list-item-title>
+            <v-list-item-subtitle>
+              {{ info.date }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-dialog v-model="dialog" max-width="600">
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  class="dialog-btn ma-2"
+                  color="primary lighten-1"
+                  v-bind="attrs"
+                  icon
+                  v-on="on"
+                >
+                  <v-icon color="grey lighten-1"
+                    >mdi-arrow-right-circle-outline</v-icon
                   >
-                    <v-icon color="grey lighten-1"
-                      >mdi-arrow-right-circle-outline</v-icon
-                    >
-                  </v-btn>
-                </template>
-                <template #default="dialog">
-                  <v-card>
-                    <v-system-bar color="primary" dark> News </v-system-bar>
-                    <v-card-title>
-                      {{ info.title }}
-                    </v-card-title>
-                    <v-card-subtitle>
-                      {{ info.date }}
-                    </v-card-subtitle>
-                    <v-card-text class="mt-6" v-html="info.contents">
-                    </v-card-text>
-                    <v-card-actions v-if="info.actions">
-                      <base-btn-inside
-                        v-for="btn in info.actions"
-                        :key="btn.id"
-                        :link="btn.link"
-                        :to="btn.to"
-                      ></base-btn-inside>
-                    </v-card-actions>
-                    <v-card-actions v-if="info.openinnew">
-                      <base-btn-open-in-new
-                        v-for="btn in info.openinnew"
-                        :key="btn.id"
-                        :link="btn.link"
-                        :url="btn.url"
-                      ></base-btn-open-in-new>
-                    </v-card-actions>
-                    <v-card-actions class="justify-end">
-                      <v-btn text @click="dialog.value = false">Close</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </template>
-              </v-dialog>
-            </v-list-item-action>
-          </v-list-item>
-        </v-skeleton-loader>
+                </v-btn>
+              </template>
+              <template #default="dialog">
+                <v-card>
+                  <v-system-bar color="primary" dark> News </v-system-bar>
+                  <v-card-title>
+                    {{ info.title }}
+                  </v-card-title>
+                  <v-card-subtitle>
+                    {{ info.date }}
+                  </v-card-subtitle>
+                  <v-card-text class="mt-6" v-html="info.contents">
+                  </v-card-text>
+                  <v-card-actions v-if="info.actions">
+                    <base-btn-inside
+                      v-for="btn in info.actions"
+                      :key="btn.id"
+                      :link="btn.link"
+                      :to="btn.to"
+                    ></base-btn-inside>
+                  </v-card-actions>
+                  <v-card-actions v-if="info.openinnew">
+                    <base-btn-open-in-new
+                      v-for="btn in info.openinnew"
+                      :key="btn.id"
+                      :link="btn.link"
+                      :url="btn.url"
+                    ></base-btn-open-in-new>
+                  </v-card-actions>
+                  <v-card-actions class="justify-end">
+                    <v-btn text @click="dialog.value = false">Close</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
+          </v-list-item-action>
+        </v-list-item>
       </template>
     </v-list>
     <v-card-text v-if="pagination">
@@ -119,3 +117,10 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.wrap-text {
+  word-break: break-all;
+  white-space: normal;
+}
+</style>
