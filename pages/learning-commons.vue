3755cc6c0de5434645cbbc09d53663f32105e00c
shu-saginoya <saginoya@create-nippo.co.jp>
2021-11-02 15:31:53 +0900

    ラーニングコモンズページの制作

---
+++ pages/learning-commons.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <base-page-title>ラーニングコモンズ</base-page-title>
+    <v-row dense>
+      <v-col v-for="(item, i) in items" :key="i" cols="6">
+        <v-card :href="item.href" target="_blank" dark>
+          <v-img
+            :src="item.img"
+            class="align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+            <v-card-title>{{ item.title }}<v-icon class="ml-2">mdi-open-in-new</v-icon></v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        title: 'ラーニングコモンズの紹介',
+        href: 'https://www.ferris.ac.jp/information/library/learningcommons.html',
+        img: require(`@/assets/image/learning-commons-01.jpg`),
+      },
+      {
+        title: 'ピアノの弾ける図書館',
+        href: 'https://www.ferris.ac.jp/blog/20210202/7203/',
+        img: require(`@/assets/image/learning-commons-02.jpg`),
+      },
+    ],
+  }),
+}
+</script>
