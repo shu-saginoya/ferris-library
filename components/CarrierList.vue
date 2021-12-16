a19b727ab3e97a30ad7be889027a6000e2be69e5
shu-saginoya <saginoya@create-nippo.co.jp>
2021-12-16 16:10:43 +0900

    キャリア支援のページを作成

---
+++ components/CarrierList.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row dense>
+    <v-col v-for="(item, i) in items" :key="'item' + i" cols="12" lg="6">
+      <v-card>
+        <v-card-title>
+          {{ item.name }}
+        </v-card-title>
+        <v-card-subtitle>
+          {{ item.type }}
+        </v-card-subtitle>
+        <v-card-text>
+          {{ item.content }}
+          <p v-if="item.logout" class="mb-0">
+            <span class="orange--text ml-2">
+              <v-icon small color="orange">mdi-alert-circle-outline</v-icon>
+              利用後は必ずログアウトしてください
+            </span>
+          </p>
+        </v-card-text>
+        <div class="d-flex">
+          <v-card-text>
+            <v-chip small class="mr-2">アクセス</v-chip>{{ item.access }}
+          </v-card-text>
+          <v-card-actions v-if="item.links">
+            <v-btn
+              v-for="(link, j) in item.links"
+              :key="'link' + j"
+              text
+              :href="link.url"
+              target="_blank"
+            >
+              {{ link.name }}<v-icon right>mdi-open-in-new</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'CarrierList',
+  props: {
+    items: { type: Array, required: true },
+  },
+  data: () => ({}),
+}
+</script>
---
+++ pages/carrier.vue
@@ -0,0 +1,255 @@
+<template>
+  <v-container>
+    <base-page-title>{{ title }}</base-page-title>
+    <p>
+      図書館のキャリア支援のコーナーです。就職活動に役立つ契約データベースや図書館資料を紹介します。
+    </p>
+    <v-tabs v-model="tab" fixed-tabs show-arrows>
+      <v-tab>企業や業種を研究する</v-tab>
+      <v-tab>最新の時事情報をつかむ</v-tab>
+      <v-tab>職種や資格について調べる</v-tab>
+      <v-tab>OPACで関連本を集める</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-container>
+          <base-sub-title>企業や業種を研究する</base-sub-title>
+          <carrier-list :items="items1"></carrier-list>
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
+          <base-sub-title>最新の時事情報をつかむ</base-sub-title>
+          <carrier-list :items="items2"></carrier-list>
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
+          <base-sub-title>職種や資格について調べる</base-sub-title>
+          <carrier-list :items="items3"></carrier-list>
+        </v-container>
+      </v-tab-item>
+      <v-tab-item>
+        <v-container>
+          <base-sub-title>OPACで関連本を集める</base-sub-title>
+          <p>
+            「就職」「キャリア」「職業」「業種」「企業」「仕事」「就活」などのキーワードで検索してみましょう。
+          </p>
+          <base-btn-my-library></base-btn-my-library>
+          <v-timeline dense>
+            <v-timeline-item>
+              <v-img
+                :src="require(`@/assets/image/carrier-01.gif`)"
+                max-width="676"
+                contain
+              ></v-img>
+              <p class="text-caption">部分一致検索にキーワードを入れて検索</p>
+            </v-timeline-item>
+            <v-timeline-item>
+              <v-img
+                :src="require(`@/assets/image/carrier-02.gif`)"
+                max-width="676"
+                contain
+              ></v-img>
+              <p class="text-caption">情報がヒット</p>
+            </v-timeline-item>
+          </v-timeline>
+        </v-container>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
+</template>
+
+<script>
+import BaseBtnMyLibrary from '~/components/BaseBtnMyLibrary.vue'
+import BaseSubTitle from '~/components/BaseSubTitle.vue'
+export default {
+  components: { BaseSubTitle, BaseBtnMyLibrary },
+  data: () => ({
+    title: 'キャリア支援',
+    tab: null,
+    items1: [
+      {
+        name: 'JapanKnowledge Lib',
+        type: 'データベース',
+        logout: true,
+        content:
+          '「会社四季報」や「週刊エコノミスト」など、就活に役立つコンテンツを一括検索できます。（データ更新：毎週）',
+        access: '学内PCから',
+        links: [
+          {
+            name: 'アクセス',
+            url: 'http://japanknowledge.com/library/'
+          }
+        ]
+      },
+      {
+        name: 'magazineplus',
+        type: 'データベース',
+        logout: true,
+        content:
+          '「週刊東洋経済」「一橋ビジネスレビュー」などのビジネス誌のタイトル情報が収録されています。（データ更新：毎週）',
+        access: '学内PCから',
+        links: [
+          {
+            name: 'アクセス',
+            url: 'http://web.nichigai.co.jp/nga_presen/entrance.do?userName=FRA&mngConfId=FRA&page=initializeMagazine.do'
+          }
+        ]
+      },
+      {
+        name: '日経テレコン21',
+        type: 'データベース',
+        logout: false,
+        content:
+          '「日本経済新聞」「日経産業新聞」「日経（MJ）流通新聞」（1975年4月から）の記事や企業情報、人事情報などを検索・閲覧できます。（データ更新：毎日）',
+        access: '学内PCから',
+        links: [
+          {
+            name: 'アクセス',
+            url: 'http://www.db-month.biglobe.ne.jp/cgi-bin/telcom21/ferris/enter.cgi'
+          }
+        ]
+      },
+    ],
+    items2: [
+      {
+        name: '聞蔵Ⅱビジュアル',
+        type: 'データベース',
+        logout: true,
+        content:
+          '1985年から当日までの「朝日新聞」「AERA」「週刊朝日」の記事を同時に検索・閲覧できます。「ナビ検索」を選択すると就活関連記事を一括で抽出することができます。（データ更新：毎日）',
+        access: '学内PCから',
+        links: [
+          {
+            name: 'アクセス',
+            url: 'https://database.asahi.com/login/loginexec.php?user_id=fej248gyk&password=bby046wwt'
+          }
+        ]
+      },
+      {
+        name: '毎索',
+        type: 'データベース',
+        logout: true,
+        content:
+          '創刊号（1872年）からの「毎日新聞」「週刊エコノミスト」「Mainichi Daily News」の記事を検索・閲覧できます。（データ更新：毎日）',
+        access: '学内PCから',
+        links: [
+          {
+            name: 'アクセス',
+            url: 'https://dbs.g-search.or.jp/WMAI/WMAI_ipcu_login.html'
+          }
+        ]
+      },
+      {
+        name: '日経テレコン21',
+        type: 'データベース',
+        logout: false,
+        content:
+          '「日本経済新聞」「日経産業新聞」「日経（MJ）流通新聞」（1975年4月から）の記事や企業情報、人事情報などを検索・閲覧できます。（データ更新：毎日）',
+        access: '学内PCから',
+        links: [
+          {
+            name: 'アクセス',
+            url: 'http://www.db-month.biglobe.ne.jp/cgi-bin/telcom21/ferris/enter.cgi'
+          }
+        ]
+      },
+      {
+        name: 'ヨミダス歴史館',
+        type: 'データベース',
+        logout: true,
+        content:
+          '創刊号（1874年）からの読売新聞と、THE JAPAN NEWS（英字新聞）の記事を検索・閲覧できます。就活に特化したコーナーがあります。（データ更新：毎日）',
+        access: '学内PCから',
+        links: [
+          {
+            name: 'アクセス',
+            url: 'https://database.yomiuri.co.jp/rekishikan/'
+          }
+        ]
+      },
+      {
+        name: 'AERA',
+        type: '雑誌',
+        logout: false,
+        content:
+          '政治、経済、生活関連のニュース週刊誌です。（保存期間1年）（「聞蔵Ⅱビジュアル」からも検索・閲覧できます）',
+        access: '緑園本館2階雑誌コーナー',
+        links: [
+          {
+            name: '聞蔵Ⅱビジュアル',
+            url: 'https://database.asahi.com/login/loginexec.php?user_id=fej248gyk&password=bby046wwt'
+          }
+        ]
+      },
+      {
+        name: '週刊エコノミスト',
+        type: '雑誌',
+        logout: false,
+        content:
+          '経済、外交、社会問題について専門家が分析する総合ビジネス誌です。（保存期間1年）（「Japanknowledge Lib」からも検索・閲覧できます）',
+        access: '緑園本館2階雑誌コーナー',
+        links: [
+          {
+            name: 'Japanknowledge Lib',
+            url: 'http://japanknowledge.com/library/'
+          }
+        ]
+      },
+      {
+        name: '週刊金曜日',
+        type: '雑誌',
+        logout: false,
+        content:
+          '人権、市民運動、平和、環境などを考察しています。（保存期間1年）',
+        access: '緑園本館2階雑誌コーナー',
+      },
+      {
+        name: '週刊東洋経済',
+        type: '雑誌',
+        logout: false,
+        content: '最もスタンダードな経済ビジネス情報誌です。（保存期間1年）',
+        access: '緑園本館2階雑誌コーナー',
+      },
+      {
+        name: '日経WOMAN',
+        type: '雑誌',
+        logout: false,
+        content: '働く女性のためのビジネス情報誌です。（保存期間1年）',
+        access: '緑園本館2階雑誌コーナー',
+      },
+      {
+        name: 'Newsweek日本版',
+        type: '雑誌',
+        logout: false,
+        content: '世界中で発行されている国際ニュース誌の日本版です。',
+        access: '緑園本館2階雑誌コーナー',
+      },
+      {
+        name: '一橋ビジネスレビュー',
+        type: '雑誌',
+        logout: false,
+        content:
+          '経営マネジメント誌です。経営者インタビューなどを掲載しています。',
+        access: '緑園本館2階雑誌コーナー',
+      },
+    ],
+    items3: [
+      {
+        name: '「なるにはbooks」シリーズ',
+        type: '図書',
+        logout: true,
+        content:
+          '専門家の視点からその仕事の魅力を紹介する職業ガイドブックシリーズです。',
+        access: '緑園図書館3階 請求記号（背ラベルの番号）：366.29||N53',
+      },
+    ],
+  }),
+  head() {
+    return {
+      title: this.title,
+    }
+  },
+}
+</script>
