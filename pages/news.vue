8eb8859892b08783179a91d6a2f0f0e5d944cdca
shu-saginoya <saginoya@create-nippo.co.jp>
2021-12-16 13:36:09 +0900

    コンポーネントへの引数の渡し方を修正

--- pages/news.vue
+++ pages/news.vue
@@ -3,7 +3,7 @@
     <base-page-title>{{ title }}</base-page-title>
     <v-row>
       <v-col cols="12" md="10" lg="8" xl="6">
-        <news-list pagination="true"
+        <news-list :pagination="true"
           ><template #name>ニュース一覧</template></news-list
         >
       </v-col>
