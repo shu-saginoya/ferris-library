ac0c4d94517040e7ff5ae3acd3aaf6e33ed323c8
shu-saginoya <saginoya@create-nippo.co.jp>
2022-01-24 15:43:12 +0900

    英語のカレンダーページを制作

---
+++ pages/english/calender.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <text-page-title>{{ title }}</text-page-title>
+    <v-row>
+      <v-col cols="12" xl="8">
+        <calender english-page="true"></calender>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'english',
+  data: () => ({
+    title: 'Calendar',
+
+  }),
+  head() {
+    return {
+      title: this.title,
+    }
+  },
+}
+</script>
