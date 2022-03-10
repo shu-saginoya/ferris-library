4e3438ebd8f5b5b5cceb5ccddb9708dbe9d6b97f
shu-saginoya <saginoya@create-nippo.co.jp>
2022-03-10 11:48:01 +0900

    英語カレンダーページのコンポーネント呼び出しの修正

--- pages/english/calender.vue
+++ pages/english/calender.vue
@@ -3,7 +3,7 @@
     <text-page-title>{{ title }}</text-page-title>
     <v-row>
       <v-col cols="12" xl="8">
-        <calender :english-page="true"></calender>
+        <the-calender :english-page="true"></the-calender>
       </v-col>
     </v-row>
   </v-container>
