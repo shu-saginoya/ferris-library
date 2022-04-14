fe2731c371b895bd973294598bcd112e526d375b
shu-saginoya <saginoya@create-nippo.co.jp>
2022-04-14 16:43:00 +0900

    新聞ページの更新
    PDFファイルを読み込む形式の廃止

---
+++ components/ListNewspaperDetails.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-simple-table>
+    <tbody>
+      <tr
+        v-for="(value, name, index) in items"
+        v-show="showValue(value, name)"
+        :key="index"
+      >
+        <th>{{ displayTitle(name) }}</th>
+        <td v-html="value"></td>
+      </tr>
+    </tbody>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: 'ListNewspaperDetails',
+  props: {
+    items: { type: Object, required: true },
+  },
+  data: () => ({}),
+  methods: {
+    displayTitle(name) {
+      switch (name) {
+        case 'country':
+          return '発行国'
+        case 'publisher':
+          return '発行元'
+        case 'cycle':
+          return '刊行頻度'
+        case 'retentionPeriod':
+          return '所蔵状況'
+        case 'location':
+          return '所在'
+        case 'note':
+          return '収録データベース・備考'
+        default:
+          return name
+      }
+    },
+    showValue(value, name) {
+      const flag = value !== '' && name !== 'title' && name !== 'subTitle'
+      return flag
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- pages/newspaper.vue
+++ pages/newspaper.vue
@@ -1,9 +1,39 @@
 <template>
   <v-container>
-    <text-page-title>{{ title }}</text-page-title>
+    <v-col cols="12" lg="10" xl="6">
+      <text-page-title>{{ title }}</text-page-title>
+      <p class="update-date">{{ updateDate }}</p>
+    </v-col>
     <v-row>
-      <v-col cols="12" lg="10" xl="6">
-        <display-pdf :location="location" :name="name"></display-pdf>
+      <v-col
+        v-for="(item, index) in items"
+        :key="'item' + index"
+        cols="12"
+        lg="10"
+        xl="6"
+      >
+        <text-sub-title>
+          {{ item.category }}
+        </text-sub-title>
+        <v-expansion-panels accordion focusable>
+          <v-expansion-panel
+            v-for="(newspaper, index2) in item.newspapers"
+            :key="'newspaper' + index2"
+          >
+            <v-expansion-panel-header>
+              <span v-if="!newspaper.subTitle">
+                <v-icon left>mdi-newspaper</v-icon>
+                {{ newspaper.title }}
+              </span>
+              <span v-else class="newspaper-sub-title">
+                {{ newspaper.subTitle }}
+              </span>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <list-newspaper-details :items="newspaper"></list-newspaper-details>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-col>
     </v-row>
   </v-container>
@@ -13,9 +43,288 @@
 export default {
   name: 'PageNewspaper',
   data: () => ({
-    title: '新聞（緑園キャンパス）',
-    location: '/documents/newspaper.pdf',
-    name: 'newspaper.pdf',
+    title: '緑園本館所蔵新聞リスト',
+    updateDate: '2022年4月現在',
+    items: [
+      {
+        category: '日本語（全国紙・地方紙）',
+        newspapers: [
+          {
+            title: '朝日新聞',
+            subTitle: '',
+            country: '',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '・朝日クロスサーチ（オンライン）<br />1879（明12）～継続中',
+          },
+          {
+            title: '朝日新聞',
+            subTitle: '朝日新聞：復刻版（明治・大正編）',
+            country: '',
+            publisher: '',
+            cycle: '',
+            retentionPeriod: '1888（明21）7月～1926（大15 *昭和元年)',
+            location: '5F書庫',
+            note: '',
+          },
+          {
+            title: '朝日新聞',
+            subTitle: '朝日新聞：復刻版（昭和編）',
+            country: '',
+            publisher: '',
+            cycle: '',
+            retentionPeriod: '1927（昭和2）～1955（昭和30）',
+            location: '5F書庫',
+            note: '',
+          },
+          {
+            title: '朝日新聞',
+            subTitle: '朝日新聞 縮刷版',
+            country: '',
+            publisher: '',
+            cycle: '',
+            retentionPeriod: '1966（昭和41）, 1988（昭63）～継続中',
+            location: '1F雑誌・5F書庫',
+            note: '当年度以前は5階書庫。 利用の際は2階カウンターに申し込み。',
+          },
+          {
+            title: '朝日新聞',
+            subTitle: '朝日新聞.マイクロフィルム',
+            country: '',
+            publisher: '',
+            cycle: '',
+            retentionPeriod:
+              '1935（昭和10）～1942（昭和17）, 1944（昭19）～1945（昭20）, 1947（昭22）～1954（昭29）（欠号あり）',
+            location: '1F貴重書庫',
+            note: '利用の際は2階カウンターに申し込み。',
+          },
+          {
+            title: '神奈川新聞',
+            subTitle: '',
+            country: '',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '産経新聞',
+            subTitle: '',
+            country: '',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '東京新聞',
+            subTitle: '',
+            country: '',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '東京新聞',
+            subTitle: 'みやこ新聞・都新聞（東京新聞の前身）：復刻版',
+            country: '',
+            publisher: '',
+            cycle: '',
+            retentionPeriod: '1888(明21）11月～1937(昭12年)6月　（欠号あり）',
+            location: '1F雑誌',
+            note: '',
+          },
+          {
+            title: '日本経済新聞',
+            subTitle: '',
+            country: '',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '・日経テレコン21（オンライン）<br />1981(昭56）～継続中<br />・日経全文記事データベース（CD-ROM)<br />1995（平7）～2010(平22) 《2FCD-ROM端末》',
+          },
+          {
+            title: '毎日新聞',
+            subTitle: '',
+            country: '',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '・毎索（オンライン）1872（明5）～継続中',
+          },
+          {
+            title: '毎日新聞',
+            subTitle: '毎日新聞 縮刷版',
+            country: '',
+            publisher: '',
+            cycle: '',
+            retentionPeriod: '1967（昭和42)2月～1988(昭和63）1月',
+            location: '5F書庫',
+            note: '',
+          },
+          {
+            title: '毎日新聞',
+            subTitle: '毎日新聞（横濱毎日新聞）：復刻版',
+            country: '',
+            publisher: '',
+            cycle: '',
+            retentionPeriod: '1870（明3）～1906（昭和63）6月',
+            location: '5F書庫',
+            note: '',
+          },
+          {
+            title: '読売新聞',
+            subTitle: '',
+            country: '',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '・ヨミダス歴史館（オンライン）<br />1874（明7）～継続中<br />・明治／大正／昭和の讀賣新聞（CD-ROM）<br />1874（明7）-1960（昭35） 《2FCD-ROM端末》',
+          },
+        ],
+      },
+      {
+        category: '外国語',
+        newspapers: [
+          {
+            title:
+              'Die Zeit : Wochenzeitung für Politik, Wirtschaft, Handel und Kultur',
+            subTitle: '',
+            country: 'ドイツ',
+            publisher: '',
+            cycle: '週刊',
+            retentionPeriod: '最新3年分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title:
+              'International New York Times : DISTRIBUTED WITH The Japan Times',
+            subTitle: '',
+            country: '日本',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: 'The Japan Times',
+            subTitle: '',
+            country: '日本',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '중앙 일보. （中央日報）',
+            subTitle: '',
+            country: '韓国',
+            publisher: '',
+            cycle: '日刊',
+            retentionPeriod: '最新3か月分',
+            location: '2F新聞',
+            note: '',
+          },
+        ],
+      },
+      {
+        category: '専門紙・その他',
+        newspapers: [
+          {
+            title: '環境新聞',
+            subTitle: '',
+            country: '',
+            publisher: '環境新聞社',
+            cycle: '週刊',
+            retentionPeriod: '最新1年分',
+            location: '2F雑誌',
+            note: '',
+          },
+          {
+            title: '教育学術新聞',
+            subTitle: '',
+            country: '',
+            publisher: '教育学術新聞',
+            cycle: '週刊',
+            retentionPeriod: '最新1年分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: 'キリスト教学校教育',
+            subTitle: '',
+            country: '',
+            publisher: 'キリスト教学校教育同盟',
+            cycle: '月刊',
+            retentionPeriod: '最新1年分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '新聞協会報',
+            subTitle: '',
+            country: '',
+            publisher: '日本新聞協会',
+            cycle: '週刊',
+            retentionPeriod: '最新1年分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '週刊読書人',
+            subTitle: '',
+            country: '',
+            publisher: '読書人',
+            cycle: '週刊',
+            retentionPeriod: '最新1年分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '図書新聞',
+            subTitle: '',
+            country: '',
+            publisher: '図書新聞',
+            cycle: '週刊',
+            retentionPeriod: '最新1年分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '日本教育新聞',
+            subTitle: '',
+            country: '',
+            publisher: '日本教育新聞社',
+            cycle: '週刊',
+            retentionPeriod: '最新1年分',
+            location: '2F新聞',
+            note: '',
+          },
+          {
+            title: '民間放送',
+            subTitle: '',
+            country: '',
+            publisher: '日本民間放送連盟',
+            cycle: '旬刊',
+            retentionPeriod: '最新1年分',
+            location: '2F新聞',
+            note: '',
+          },
+        ],
+      },
+    ],
   }),
   head() {
     return {
@@ -24,3 +333,14 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.newspaper-sub-title {
+  color: grey;
+  padding-left: 2em;
+}
+.update-date {
+  text-align: right;
+  color: grey;
+}
+</style>
