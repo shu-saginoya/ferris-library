f1111afb3531ac0dd0cda950068c37f953301bf4
shu-saginoya <saginoya@create-nippo.co.jp>
2022-04-21 14:23:48 +0900

    企画展示ページの制作

---
+++ pages/exhibition.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <text-page-title>{{ title }}</text-page-title>
+    <v-row>
+      <v-col cols="12" lg="8" xl="6">
+        <p>text</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'PageExhibition',
+  data: () => ({
+    title: '企画展示',
+  }),
+  head() {
+    return {
+      title: this.title,
+    }
+  },
+}
+</script>
