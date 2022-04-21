0e9e2f87a1d8744ac25b3b0b9e8656af18557d2e
shu-saginoya <saginoya@create-nippo.co.jp>
2022-04-21 14:53:12 +0900

    企画展示ページの更新
    microCMSの基本的な読み込みコードの追加

--- pages/exhibition.vue
+++ pages/exhibition.vue
@@ -3,7 +3,13 @@
     <text-page-title>{{ title }}</text-page-title>
     <v-row>
       <v-col cols="12" lg="8" xl="6">
-        <p>text</p>
+        <ul>
+          <li v-for="content in contents" :key="content.id">
+            <nuxt-link :to="`/${content.id}`">
+              {{ content.title }}
+            </nuxt-link>
+          </li>
+        </ul>
       </v-col>
     </v-row>
   </v-container>
@@ -12,6 +18,12 @@
 <script>
 export default {
   name: 'PageExhibition',
+  async asyncData({ $microcms }) {
+    const data = await $microcms.get({
+      endpoint: 'exhibition',
+    })
+    return data
+  },
   data: () => ({
     title: '企画展示',
   }),
