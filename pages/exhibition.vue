7d6ddfe2fb78d57d09eb9f3d0c0d3cca5c21954c
shu-saginoya <saginoya@create-nippo.co.jp>
2022-04-22 13:32:16 +0900

    企画展示ページの一覧の作成

--- pages/exhibition.vue
+++ pages/exhibition.vue
@@ -2,14 +2,34 @@
   <v-container>
     <text-page-title>{{ title }}</text-page-title>
     <v-row>
-      <v-col cols="12" lg="8" xl="6">
-        <ul>
-          <li v-for="content in contents" :key="content.id">
-            <nuxt-link :to="`/${content.id}`">
-              {{ content.title }}
-            </nuxt-link>
-          </li>
-        </ul>
+      <v-col
+        v-for="content in contents"
+        :key="content.id"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        xl="2"
+      >
+        <v-card height="100%">
+          <v-img
+            :src="content.eyecatch ? content.eyecatch.url : ''"
+            height="160px"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.8)"
+          >
+            <v-card-text>
+              <v-chip v-if="content.category" color="primary">
+                {{ content.category.name }}
+              </v-chip>
+            </v-card-text>
+          </v-img>
+          <v-card-title>
+            {{ content.title }}
+          </v-card-title>
+          <v-card-subtitle>
+            {{ $dayjs(content.publishedAt).format('YYYY年M月D日') }}
+          </v-card-subtitle>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -21,11 +41,13 @@ export default {
   async asyncData({ $microcms }) {
     const data = await $microcms.get({
       endpoint: 'exhibition',
+      queries: {orders: 'createdAt'},
     })
     return data
   },
   data: () => ({
     title: '企画展示',
+    show: false,
   }),
   head() {
     return {
