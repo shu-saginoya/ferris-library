2b8d5fce8bd78f02467887985e6e504b3e706346
shu-saginoya <saginoya@create-nippo.co.jp>
2022-05-23 16:03:18 +0900

    NEWSページの構成変更

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,9 +1,8 @@
 <template>
   <v-container>
-    <text-page-title>{{ title }}</text-page-title>
     <v-row>
       <v-col cols="12" md="10" lg="8" xl="6">
-          <card-news :news-card="contents"></card-news>
+          <list-news :pagination="true" :contents="contents"></list-news>
       </v-col>
     </v-row>
   </v-container>
@@ -11,7 +10,7 @@
 
 <script>
 export default {
-  name: 'CmsPreview',
+  name: 'NewsDetails',
   async asyncData({ params, $microcms, $preview }) {
     const data = await $microcms.get({
       endpoint: 'news',
@@ -21,7 +20,7 @@ export default {
     return data
   },
   data: () => ({
-    title: '記事のプレビュー',
+    title: 'News',
   }),
   head() {
     return {
